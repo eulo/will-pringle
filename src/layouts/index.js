e00b326896eba8f90c4f28a08ab77fc08082a3aa
eulo <hello@will-pringle.com>
2020-03-29 10:26:05 +1100

    build error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/header'
+import Footer from '../components/footer'
 import WebGLCanvas from '../components/webgl-canvas'
 
 import '../assets/scss/main.scss'
